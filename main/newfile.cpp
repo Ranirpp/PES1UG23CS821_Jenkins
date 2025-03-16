96b6259f4caeb992561fa3c70818883e7529315d
Ranirpp <patilrani621@gmail.com>
2025-03-16 22:46:24 +0530

    Create newfile.cpp

---
+++ main/newfile.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, This is PES1UG23CS821\n";
+  cout << "I have successfully built and completed Jenkins Lab \n";
+  return 0;
+}
